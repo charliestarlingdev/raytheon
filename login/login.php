702cada066a15817819bec84d7e799c3e4d2c834
Charlie Starling <charliejackstarling1@gmail.com>
2025-02-17 13:13:10 +0000

    login setup

--- login/login.php
+++ login/login.php
@@ -3,11 +3,24 @@
 
 <head>
     <title>Raytheon :: Login</title>
+    <link rel="icon" type="image/gif/png" href="../images/logo.png">
+    <link rel="stylesheet" href="../styles/desktop.css">
 </head>
 
 <body>
-    <h1>Raytheon Login</h1>
-    
+    <div class="logo-container">
+        <img src="../images/logo.png" alt="RTX :: Raytheon">
+    </div>
+    <div class="main">
+        <h1>Sign In</h1>
+        <div class= "form-container">
+            <form action="#">
+                <input type="text" placeholder="Username" name="username" required>
+                <input type="password" placeholder="Password" name="username" required>
+                <button type="submit" class="submit-btn">Login</button>
+            </form>
+        </div>
+    </div>
 </body>
 
 </html>
