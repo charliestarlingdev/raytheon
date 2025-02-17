2a2afec7adb624b8b96b51c151a6e17ef915b232
Charlie Starling <charliejackstarling1@gmail.com>
2025-02-17 16:24:10 +0000

    sections

--- home.php
+++ home.php
@@ -3,15 +3,28 @@
 
 <head>
     <title>Home Page</title>
-    <link rel="stylesheet" href="../styles/desktop.css">
+    <link rel="stylesheet" href="styles/desktop.css">
 
     <?php include "./includes/navbar.php" ?>
 
 </head>
 
 <body>
-    <h1>Raytheon Home Page</h1>
-    <p>I'd probably have a map here from somewhere.</p> <!-- Leaflet -->
+
+<div class="content-wrapper">
+    <div class="primary">
+    
+    </div>
+
+    <div class="secondary">
+
+    </div>
+
+    <div class="tertiary">
+
+   </div>
+</div>
+
 </body>
 
 </html>
