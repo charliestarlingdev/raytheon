c6ecf05b14a3349e45bc71360666dc4dc633bd56
Charlie Starling <charliejackstarling1@gmail.com>
2025-02-17 11:58:27 +0000

    setup

---
+++ home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Home Page</title>
+</head>
+
+<body>
+    <h1>Raytheon Home Page</h1>
+    <p>I'd probably have a map here from somewhere.</p> <!-- Leaflet -->
+</body>
+
+</html>
---
+++ login/login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Raytheon :: Login</title>
+</head>
+
+<body>
+    <h1>Raytheon Login</h1>
+    
+</body>
+
+</html>
